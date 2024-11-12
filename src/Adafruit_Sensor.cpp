9895ab5e865a7ea0c2cadf477158b5ae5d1e5d4b
Blaine Rothrock <blaine.p.rothrock@gmail.com>
2024-11-12 17:58:39 -0500

    src dir for particle

---
+++ src/Adafruit_Sensor.cpp
@@ -0,0 +1,11 @@
+#include "Adafruit_Sensor.h"
+
+/**************************************************************************/
+/*!
+    @brief  Prints sensor information to serial console
+*/
+/**************************************************************************/
+void Adafruit_Sensor::printSensorDetails(void) {
+  sensor_t sensor;
+  getSensor(&sensor);
+}
